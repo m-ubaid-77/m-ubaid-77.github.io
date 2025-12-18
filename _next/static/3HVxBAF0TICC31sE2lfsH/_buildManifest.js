919717de182ac219fbcff8ee00022906b2ef0dbb
Muhammad Ubaid <m.ubaid.77@gmail.com>
2025-12-18 14:42:07 +0500

    updated assets path

--- _next/static/3HVxBAF0TICC31sE2lfsH/_buildManifest.js
+++ _next/static/3HVxBAF0TICC31sE2lfsH/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/m-ubaid-77.github.io//_next/:path+",
-        "destination": "/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
